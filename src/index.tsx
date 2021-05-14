d99f6902d92cceff71fc57f0a5d046a4583a07c6
zihaoguan <zgua779@aucklanduni.ac.nz>
2021-05-14 20:56:55 +1200

    removed unnecessary imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
-import allReducer from "./store/reducers/";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/index";
-import { fetchAgents } from "./store/actions";
-
-// /* eslint-disable no-underscore-dangle */
-// const store = createStore(
-//   allReducer,
-//   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-// );
-// /* eslint-enable */
 
 ReactDOM.render(
   <React.StrictMode>
